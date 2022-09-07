840f6ce38f1e01c84e0367b81b28a84a76fdf160
jsong73 <jessicajinsong@gmail.com>
2022-09-07 00:28:39 -0400

    updating my pw generator

--- brainstorm.md
+++ brainstorm.md
@@ -5,7 +5,7 @@ Once user clicks the button, a PROMPT BOX will ask how many characters the pw sh
     what happens if you press cancel? 
     what happens if you input anything less than 8 characthers? -->
 
-Once user types in a number ABOVE 8 and less than 128, a CONFIRM BOX will ask to click OK to confirm including special characthers.
+Once user types in a number ABOVE 8 and less than 128, a CONFIRM BOX will ask to click OK to confirm including special characters.
 <!-- OK = special characters
 CANCEL = no special characters -->
 
@@ -20,6 +20,13 @@ CANCEL = no lowercase characters -->
 Once user confirms OK or CANCEL, another CONFIRM BOX will ask to confirm if you want to include uppercase characters.
 <!-- OK = uppercase characters
 CANCEL = no uppercase characters -->
+
+The user has chosen the conditions of what they want to add to the password.
+=> I need something to iterate multiple times. (A for loop?) 
+starting point is zero or 1
+Increment by 1
+What will the terminating condition be? 10
+
 
 Finally a password will be generated in a DASHED TEXT BOX with the selected requirements.
 
--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 // array of special characters that can be used
 var symbolList = [
     '@',
@@ -102,16 +103,16 @@ var symbolList = [
     'Z',
   ];
 
-  function randomNumber(arr){
-    return Math.floor(math.random()*arr.length);
   
-  }
+
   // shows whatever user inputs into PROMPT text box in console
 function generatePassword(){
      var userInput= window.prompt("How many characters would you like your password to contain?");
-     console.log(userInput);
+     console.log(userInput); //this is a string
 
      // Variable to store length of pw from user input
+     //parse TO an integer
+     //parseInt will CONVERT or PARSE the STRING into an INTEGER
      var passwordLength = parseInt(userInput)
     
     // if pw length is not a number, error will pop up and prompt will end
@@ -123,39 +124,48 @@ function generatePassword(){
     if (passwordLength < 8 || passwordLength > 128){
         alert("Password length must be between 8-28 characters!");
         return;
-    
+    }
+
+  // to create ok or cancel prompt to store bollean of symbols, numbers, upper and lowercase
+  var userWantsNumbers = window.confirm("Click ok to include numbers in your password.")
+  var userWantsSymbols = window.confirm("Click ok to include symbols in your password.")
+  var userWantsLowercase = window.confirm("Click ok to include lowercase letters in your password.")
+  var userWantsUppercase = window.confirm("Click ok to include uppercase letters in your password.")
+
+  var passwordOptionList = [];
+
+
+  if(userWantsNumbers === true) {
+    // we concatenate (join) the values from the numberList to the passwordOptionList and CREATE a new Array.
+    newArray = passwordOptionList.concat(numberList)
+  } 
+  if(userWantsSymbols === true) {
+    // we concatenate (join) the values from the symboLList to the passwordOptionList and CREATE a new Array.
+    newArray =  newArray.concat(symbolList)  
+  } 
+  if(userWantsLowercase === true) {
+    newArray = newArray.concat(lowercaseList)  
+  }
+  if(userWantsUppercase === true) {
+    newArray = newArray.concat(uppercaseList)  
+  }
+  
+  console.log(newArray);
+
 }
 
-// to create ok or cancel prompt to store bollean of symbols, numbers, upper and lowercase
-var userWantsNumbers = window.confirm("Click ok to include numbers in your password.")
-var userWantsSymbols = window.confirm("Click ok to include symbols in your password.")
-var userWantsLowercase = window.confirm("Click ok to include lowercase letters in your password.")
-var userWantsUppercase = window.confirm("Click ok to include uppercase letters in your password.")
+function getRandom(newArray){
+  var randomInt = math.floor(math.random()* newArray.length);
 
-var passwordOptionList = []
-
-
-if(userWantsNumbers === true) {
-    passwordOptionList.push(numberList)  
-}
-if(userWantsSymbols === true) {
-    passwordOptionList.push(symbolList)  
-}
-if(userWantsLowercase === true) {
-    passwordOptionList.push(lowercaseList)  
-}
-if(userWantsUppercase === true) {
-    passwordOptionList.push(uppercaseList)  
 }
 
-console.log(passwordOptionList)
-
-var generatedPassword =  ""
-}
-
-for (var i = 0;  i < passwordLength; i++) {
-  var randomItem = passwordOptionList[randomNumber(0, passwordOptionList.length)];
+console.log(getRandom(newArray));
 
 
 
-} 
+
+    //whatever LOGIC we write in this for loop
+    //will run the NUMBER of times that the user asks for. (how long the password length will be)
+    // How do we GRAB THE VALUES that the user specified and RANDOMLY scramble them
+    // Now that the newArray contains all the OPTIONS that user wants in a password, how can I RANDOMIZE those values and put them into a new string or array?
+    // And then get it to display to the page?   
